f305fc830a1e8a3b004a68d657ee6a3bbfd7a2e7
wanrizky7 <65496934+wanrizky7@users.noreply.github.com>
2020-05-17 22:03:40 +0700

    Update index.php

--- wan_chart/index.php
+++ wan_chart/index.php
@@ -100,7 +100,7 @@ var theme = {
 // tui.chart.registerTheme('myTheme', theme);
 // options.theme = 'myTheme';
 
-tui.chart.barChart(container, data, options);
+tui.chart.columChart(container, data, options);
 </script>
 
 <!--For tutorial page-->
